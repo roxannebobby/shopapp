320e74b031c79c8fafbee68bf3b7319dcd3a6e49
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-05-13 12:52:02 -0500

    secret tested may not be working properly

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 // import routes
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 // app
 const app = express();
@@ -27,6 +28,7 @@ app.use(cookieParser());
 
 // routes middleware
 app.use('/api', authRoutes);
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 8000;
 
