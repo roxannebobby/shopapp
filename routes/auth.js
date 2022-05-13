028b2c4e0c2c5f12fe47d164c4ebf3e50018e9a6
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-05-13 12:11:08 -0500

    auth refactoring complete/successful

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const {
 	signin,
 	signout,
 	requireSignin,
-} = require('../controllers/user');
+} = require('../controllers/auth');
 const { userSignupValidator } = require('../validator');
 
 // routes using router file
